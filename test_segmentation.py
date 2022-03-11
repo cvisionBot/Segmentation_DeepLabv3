fa9be86405f68ae0e3017d90becb8ea807d9a1c7
Jack Yoon <cvisionyoon@gmail.com>
2022-03-11 09:15:09 +0000

    segmentation

---
+++ test_segmentation.py
@@ -0,0 +1,95 @@
+import os
+import cv2
+import copy
+import random
+from sklearn import preprocessing
+import torch
+import argparse
+import numpy as np
+
+from models.segmentor.deeplabv3 import DeepLab
+from module.segmentator import Segmentor
+from utils.module_select import get_model
+from utils.yaml_helper import get_train_configs
+from utils.utility import preprocess_input
+
+from torchsummary import summary
+
+
+def parse_names(names_file):
+    names_file = os.getcwd()+names_file
+    with open(names_file, 'r') as f:
+        return f.read().splitlines()
+
+
+def gen_random_colors(names):
+    colors = [(random.randint(0, 255),
+               random.randint(0, 255),
+               random.randint(0, 255)) for i in range(len(names))]
+    return colors
+
+
+def decode_output(output, thresh_hold=0.5):
+    # output shape : [1, 20, 320, 320]
+    num_class = output.size()[1]
+    for i in range(num_class):
+        output[:, i, :, :] = torch.where(output[:, i, :, :] > thresh_hold, 1, 0)
+    return output
+
+def visualize_segmentation(output, idx, color, cfg):
+    # output shape : 1, 320, 320
+    # class image : 3, 320, 320
+    _, h, w = output.size()
+    image_buff = torch.zeros((cfg['in_channels'], h, w)).to('cuda')
+    output = output.expand(3, 320, 320)
+    class_image = output + image_buff
+    class_image[0, :, :] = class_image[0, :, :] * color[0]
+    class_image[1, :, :] = class_image[1, :, :] * color[1]
+    class_image[2, :, :] = class_image[2, :, :] * color[2]
+    class_image = torch.permute(class_image, (1, 2, 0))
+    return class_image
+
+
+def main(cfg, image_name, save):
+    names = parse_names(cfg['names'])
+    colors = gen_random_colors(names)
+
+    # Preprocess Image
+    image = cv2.imread(image_name)
+    image = cv2.resize(image, (cfg['input_size'], cfg['input_size']))
+    image_inp = preprocess_input(image)
+    image_inp = image_inp.unsqueeze(0)
+    if torch.cuda.is_available:
+        image_inp = image_inp.cuda()
+    
+    # Load trained model
+    backbone = get_model(cfg['backbone'])
+    model = DeepLab(Backbone=backbone, num_classes=cfg['classes'], in_channels=cfg['in_channels'])
+    if torch.cuda.is_available:
+        model = model.to('cuda')
+    
+    model_module = Segmentor.load_from_checkpoint(
+        './saved/ResNet_DeepLabv3_Pascal/version_0/checkpoints/last.ckpt',
+        model=model
+    )
+    model_module.eval()
+    # summary(model_module, input_size=(cfg['in_channels'], cfg['input_size'], cfg['input_size']))
+
+    output = model_module(image_inp)
+    output = decode_output(output)
+
+    for i in range(output.size()[1]):
+        color = colors[i]
+        class_image = visualize_segmentation(output[:, i, :, :], i, color, cfg)
+        class_image = class_image.detach().cpu().numpy()
+        cv2.imwrite('./inference/result/'+str(i)+'_class.png', class_image)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', required=True, type=str, help='Train config file')
+    parser.add_argument('--save', action='store_true', help='Train config file')
+    args = parser.parse_args()
+    cfg = get_train_configs(args.cfg)
+    
+    main(cfg, './inference/sample/test.jpg', args.save)
